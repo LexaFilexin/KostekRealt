12c317b25b7c7685ce4ba2ca5624bfb9ac03332c
LexaFilexin <lexafilexin@gmail.com>
2020-06-22 02:25:48 +0800

    site ver. alpha 1.0

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ApiController@indexAction');
+
+Route::get('/index', 'ApiController@indexAction');
+
+Route::get('/Registration', 'ApiController@RegistrationAction');
+
+Route::get('/about', 'ApiController@aboutAction');
+
+Route::get('/unnamedpage', 'ApiController@unnamedpageAction');
